8029fea11b0fd05b1b27b157de26f241077f6260
ajay <ajayzyan100@gmail.com>
2024-04-10 14:43:15 +0530

    feat(database): added 'bookings' schema

---
+++ src/db/schema/bookings.ts
@@ -0,0 +1,17 @@
+import { InferSelectModel } from 'drizzle-orm';
+import { datetime, int, mysqlTable, timestamp, varchar } from 'drizzle-orm/mysql-core';
+
+import { vehicles } from './vehicles';
+
+export const bookings = mysqlTable('bookings', {
+  id: int('id').primaryKey().autoincrement(),
+  vehicleId: int('vehicle_id').references(() => vehicles.id),
+  firstName: varchar('first_name', { length: 100 }).notNull(),
+  lastName: varchar('last_name', { length: 100 }).notNull(),
+  startDate: datetime('start_date'),
+  endDate: datetime('end_date'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().onUpdateNow().notNull(),
+});
+
+export type bookings = InferSelectModel<typeof bookings>;
