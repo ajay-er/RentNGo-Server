52e69915692fe1023f016cd57fd09a69abf3abf1
ajay <ajayzyan100@gmail.com>
2024-04-10 13:45:35 +0530

    feat(database): added 'vehicles' schema

---
+++ src/db/schema/vehicles.ts
@@ -0,0 +1,15 @@
+import { InferSelectModel } from 'drizzle-orm';
+import { boolean, int, mysqlTable, timestamp, varchar } from 'drizzle-orm/mysql-core';
+
+import { vehicleType } from './vehicleType';
+
+export const vehicles = mysqlTable('vehicles', {
+  id: int('id').primaryKey().autoincrement(),
+  typeId: int('type_id').references(() => vehicleType.id),
+  name: varchar('name', { length: 100 }).unique().notNull(),
+  isAvailable: boolean('is_available').default(true).notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().onUpdateNow().notNull(),
+});
+
+export type Vehicles = InferSelectModel<typeof vehicles>;
