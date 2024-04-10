d08bf8451cefc38cf061c60497849b0c13521b86
ajay <ajayzyan100@gmail.com>
2024-04-10 22:34:25 +0530

    fix: api issue resolved

--- src/api/vehicles/vehicleRepository.ts
+++ src/api/vehicles/vehicleRepository.ts
@@ -3,13 +3,15 @@ import { and, eq } from 'drizzle-orm';
 import connectDB from '@/db/connection';
 import { vehicles, vehicleType } from '@/db/schema';
 
-import { Vehicle, VehicleTypes } from './vehicleModel';
+import { Vehicle, VehicleCategory, VehicleTypes } from './vehicleModel';
 
 export const vehicleRepository = {
-  findAllAsync: async (page: number, limit: number): Promise<VehicleTypes[]> => {
+  findAllAsync: async (wheels: number, page: number, limit: number): Promise<VehicleTypes[]> => {
     const { DB } = await connectDB();
+    const type = wheels === 4 ? VehicleCategory.Car : VehicleCategory.Bike;
     return await DB.select()
       .from(vehicleType)
+      .where(eq(vehicleType.category, type))
       .limit(limit)
       .offset(page * limit);
   },
